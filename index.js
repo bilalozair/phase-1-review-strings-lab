cf150b030da20bddcca066512e0f5b93e2ab5efe
bilalozair <bo569@nyu.edu>
2022-03-01 13:17:56 +0500

    commit tree cf66e90f4293f72431d13dc5deb353f71a4c38ed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const shortGreeting = `"Welcome, ${currentUser[0]}!`;
+console.log(upperCaseCurrentUser);
